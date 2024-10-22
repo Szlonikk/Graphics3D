0c0da723cd8b204546fe8e4ffa0b9c3339cb3dcf
Jakub Bednarz <jakubbednarz31@gmail.com>
2024-10-23 00:10:43 +0200

    assignment 3 but get seg fault need to fix it

---
+++ src/Assignments/03_Indices/app.cpp
@@ -0,0 +1,95 @@
+//
+// Created by pbialas on 25.09.2020.
+//
+
+
+#include "app.h"
+
+#include <vector>
+
+#include "spdlog/spdlog.h"
+
+#include "glad/gl.h"
+
+#include "Application/utils.h"
+
+void SimpleShapeApplication::init() {
+    /*
+     * A utility function that reads the shaders' source files, compiles them and creates the program object,
+     * as everything in OpenGL we reference the program by an integer "handle".
+     * The input to this function is a map that associates a shader type (GL_VERTEX_SHADER and GL_FRAGMENT_SHADER) with
+     * its source file. The source files are located in the PROJECT_DIR/shaders directory, where  PROJECT_DIR is the
+     * current assignment directory (e.g., src/Assignments/Triangle).
+     */
+    auto program = xe::utils::create_program(
+            {
+                    {GL_VERTEX_SHADER,   std::string(PROJECT_DIR) + "/shaders/base_vs.glsl"},
+                    {GL_FRAGMENT_SHADER, std::string(PROJECT_DIR) + "/shaders/base_fs.glsl"}
+            });
+
+
+    if (!program) {
+        SPDLOG_CRITICAL("Invalid program");
+        exit(-1);
+    }
+
+    
+    std::vector<GLfloat> vertices = {
+        -0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f, //0
+         0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f, //1
+        -0.5f,  0.0f, 0.0f,    0.0f, 1.0f, 0.0f, //2
+         0.5f,  0.0f, 0.0f,    0.0f, 1.0f, 0.0f, //3
+        -0.5f,  0.0f, 0.0f,    1.0f, 0.0f, 0.0f, //4
+         0.5f,  0.0f, 0.0f,    1.0f, 0.0f, 0.0f, //5
+         0.0f,  0.5f, 0.0f,    1.0f, 0.0f, 0.0f, //6
+    };
+
+    std::vector<GLubyte> indices = {
+        0, 1, 2,
+        1, 3, 2,
+        4, 5, 6,
+    };
+    
+    GLuint v_buffer_handle;
+    OGL_CALL(glCreateBuffers(1, &v_buffer_handle));
+    OGL_CALL(glNamedBufferData(v_buffer_handle, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW));
+    OGL_CALL(glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle));
+    
+    
+    GLuint i_buffer_handle;
+    OGL_CALL(glCreateBuffers(1, &i_buffer_handle));
+    OGL_CALL(glNamedBufferData(i_buffer_handle, indices.size() * sizeof(GLubyte), indices.data(), GL_STATIC_DRAW));
+    OGL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, i_buffer_handle));
+
+    
+    
+
+    OGL_CALL(glGenVertexArrays(1, &vao_));
+    OGL_CALL(glBindVertexArray(vao_));
+    
+    OGL_CALL(glEnableVertexAttribArray(0));
+    OGL_CALL(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
+                                   reinterpret_cast<GLvoid *>(0)));
+
+    OGL_CALL(glEnableVertexAttribArray(1));
+    OGL_CALL(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
+                                   reinterpret_cast<GLvoid *>(3*sizeof(GLfloat))));
+
+
+
+    OGL_CALL(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    OGL_CALL(glBindVertexArray(0));
+    
+    OGL_CALL(glClearColor(0.81f, 0.81f, 0.81f, 1.0f));
+
+    auto [w, h] = frame_buffer_size();
+    OGL_CALL(glViewport(0, 0, w, h));
+
+    OGL_CALL(glUseProgram(program));
+}
+
+void SimpleShapeApplication::frame() {
+    OGL_CALL(glBindVertexArray(vao_));
+    OGL_CALL(glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(0))); 
+    OGL_CALL(glBindVertexArray(0));
+}
